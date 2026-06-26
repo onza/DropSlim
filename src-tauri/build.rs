731ad31afbc77507c44dc81699614e3c6aaca37b
onza <info@martinfarkas.com>
2026-06-26 20:14:36 +0200

    fix windows build by adjusting tauri macos-private-api config

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,6 +2,7 @@ fn main() {
     println!("cargo:rerun-if-changed=../assets/icon/icon-1024.png");
     println!("cargo:rerun-if-changed=../scripts/build-icons.mjs");
     println!("cargo:rerun-if-changed=icons/icon.png");
+    #[cfg(target_os = "macos")]
     println!("cargo:rerun-if-changed=icons/icon.icns");
     tauri_build::build()
 }
